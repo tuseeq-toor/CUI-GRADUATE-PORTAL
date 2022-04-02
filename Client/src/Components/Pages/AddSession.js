967529eadb5bd5c1e2ecc6a0a786e7f175bfeb10
tuseeq-toor <tuseeqtoor@yahoo.com>
2022-04-02 15:53:28 +0500

    scheduleSynopsisMS

--- Client/src/Components/Pages/AddSession.js
+++ Client/src/Components/Pages/AddSession.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -6,37 +6,53 @@ import Checkbox from "@mui/material/Checkbox";
 import { Button, Box } from "@mui/material";
 
 export default function AddSession() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [status, setStatus] = useState(false);
 
-  const [title,setTitle] = 
-  const handleChange=()=> {
-
-  }
-  const handleSubmit = (event) => {
-  
+  const handleTitle = (event) => {
+    setTitle(event.target.valve);
   };
+  const handleDescription = (event) => {
+    setDescription(event.target.valve);
+  };
+  const handleStatus = (event) => {
+    setStatus(event.target.checked);
+    console.log(status);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
       <TextField
         id="standard-basic"
         sx={{ width: "100%", marginBottom: "15px" }}
         label="Session Title"
+        name="title"
         color="secondary"
         variant="outlined"
-        onChange={handleChange}
+        onChange={handleTitle}
       />
 
       <TextField
         id="standard-basic"
         sx={{ width: "100%", marginBottom: "15px" }}
         label="Description"
+        name="description"
         color="secondary"
         variant="outlined"
+        onChange={handleDescription}
       />
 
       <FormGroup sx={{ marginBottom: "15px" }}>
         <FormControlLabel
+          name="status"
+          checked={status}
           control={<Checkbox color="secondary" />}
           label="Status"
+          onChange={handleStatus}
         />
       </FormGroup>
 
